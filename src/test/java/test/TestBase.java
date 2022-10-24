b4609e2e346422ffcbbd91ee47ed8f633c52a33c
19733726 <vitaliy_mayorskiy@mail.ru>
2022-10-25 00:44:27 +0300

    DZ6Objects+Fakers+Jenkins

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -8,22 +8,23 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static java.lang.String.format;
-
 public class TestBase {
+
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName", "chrome");
+//        capabilities.setCapability("browserVersion", "100.0");
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
+
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
-
-        }
-
+        Configuration.browserSize = "1920x1080";
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
 
     @AfterEach
     void addAttachments() {
